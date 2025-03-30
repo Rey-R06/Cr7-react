9bbd9aa5b4806408b2a17e9f06e5ea1b4ff0c69e
Rey-R06 <rlliiirollooo@gmail.com>
2025-03-29 23:01:27 -0500

    feat: enlace del archivo responsive a la página

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { enrutador } from './router/enrutador.jsx'
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import "./index.css"
+import "./responsive.css"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
